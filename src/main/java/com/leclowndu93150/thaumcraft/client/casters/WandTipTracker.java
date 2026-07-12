b0332447d54b13e2240ec61346498a8a910897ad
Leclowndu93150 <xdr.a123454321@gmail.com>
2026-07-12 12:57:56 +0200

    Fix wand tip capture: 26.1 hand pose is pre-inverse-rotated, drop the extra camera rotation

--- src/main/java/com/leclowndu93150/thaumcraft/client/casters/WandTipTracker.java
+++ src/main/java/com/leclowndu93150/thaumcraft/client/casters/WandTipTracker.java
@@ -18,9 +18,8 @@ public final class WandTipTracker {
 
     public static void capture(PoseStack poseStack, float tipModelY) {
         Camera camera = Minecraft.getInstance().gameRenderer.getMainCamera();
-        Vector3f local = poseStack.last().pose().transformPosition(new Vector3f(0.0F, tipModelY, 0.0F));
-        Vector3f world = camera.rotation().transform(local, new Vector3f());
-        tip = camera.position().add(world.x, world.y, world.z);
+        Vector3f offset = poseStack.last().pose().transformPosition(new Vector3f(0.0F, tipModelY, 0.0F));
+        tip = camera.position().add(offset.x, offset.y, offset.z);
         captureTime = Util.getMillis();
     }
 
