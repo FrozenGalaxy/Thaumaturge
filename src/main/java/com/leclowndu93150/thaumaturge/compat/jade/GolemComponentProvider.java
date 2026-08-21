292e19d4bcbe0e9613a119505b42cfb6e80b853a
FrozenGalaxy <156858866+FrozenGalaxy@users.noreply.github.com>
2026-08-22 00:48:53 +0200

    backport: fix golem XP showing 0 in Jade (#181)

--- src/main/java/com/leclowndu93150/thaumaturge/compat/jade/GolemComponentProvider.java
+++ src/main/java/com/leclowndu93150/thaumaturge/compat/jade/GolemComponentProvider.java
@@ -25,6 +25,6 @@ public enum GolemComponentProvider implements IEntityComponentProvider {
             return;
         }
         tooltip.add(Component.translatable(
-                "jade.thaumaturge.golem.rank", golem.getProperties().getRank(), golem.getRankXp()));
+                "jade.thaumaturge.golem.rank", golem.getProperties().getRank(), accessor.getServerData().getInt("RankXp")));
     }
 }
